1340898dc85f647f4bbfe6f7f6e152a7c9fc7567
ZCybSec <142013316+ZCybSec@users.noreply.github.com>
2024-12-19 09:56:16 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,19 +2,12 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('ZaidHijazi-Blog').then((cache) => {
             return cache.addAll([
-                // ملفات HTML
                 '/test_pwa/index.html',
                 '/test_pwa/portfolio-details.html',
                 '/test_pwa/service-details.html',
                 '/test_pwa/starter-page.html',
-                
-                // ملفات CSS
                 '/test_pwa/assets/css/main.css',
-                
-                // ملفات JS
                 '/test_pwa/assets/js/main.js',
-                
-                // صور
                 '/test_pwa/assets/img/apple-touch-icon.png',
                 '/test_pwa/assets/img/favicon.png',
                 '/test_pwa/assets/img/hero-bg.gif',
@@ -31,26 +24,18 @@ self.addEventListener('install', (event) => {
                 '/test_pwa/assets/img/portfolio/branding-3.jpg',
                 '/test_pwa/assets/img/portfolio/product-1.jpg',
                 '/test_pwa/assets/img/portfolio/product-2.jpg',
-                '/test_pwa/assets/img/portfolio/product-3.jpg',
-                
-                // ملفات الخطوط
+                '/test_pwa/assets/img/portfolio/product-3.jpg',                
                 '/test_pwa/assets/vendor/bootstrap-icons/fonts/bootstrap-icons.woff',
                 '/test_pwa/assets/vendor/bootstrap-icons/fonts/bootstrap-icons.woff2',
-        
-                // ملفات مكتبات الطرف الثالث
                 '/test_pwa/assets/vendor/aos/aos.css',
                 '/test_pwa/assets/vendor/bootstrap/css/bootstrap.min.css',
                 '/test_pwa/assets/vendor/bootstrap-icons/bootstrap-icons.css',
                 '/test_pwa/assets/vendor/glightbox/css/glightbox.min.css',
                 '/test_pwa/assets/vendor/swiper/swiper-bundle.min.css',
-        
-                // ملفات JS الخاصة بالمكتبات
                 '/test_pwa/assets/vendor/aos/aos.cjs.js',
                 '/test_pwa/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
                 '/test_pwa/assets/vendor/glightbox/js/glightbox.min.js',
                 '/test_pwa/assets/vendor/swiper/swiper-bundle.min.js',
-        
-                // ملفات أخرى مهمة
                 '/test_pwa/manifest.json',
                 '/test_pwa/service-worker.js',
                 '/test_pwa/forms/contact.php',
