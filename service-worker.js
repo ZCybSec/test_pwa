27c4c9982e5414d4ce511c543983a72ad3247fb9
ZCybSec <142013316+ZCybSec@users.noreply.github.com>
2024-12-19 10:05:30 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('Antiquities-in-Jordan-cache').then((cache) => {
+        caches.open('Blog - ZaidHijazi').then((cache) => {
             const filesToCache = [
                 // ملفات HTML
                 '/test_pwa/',
